9401ea09af5d17ac21ff5363a923d3448ff55ee0
Patrick G.S <p.g.schneider@hotmail.com>
2021-01-07 02:25:45 +0100

    up importSR_test path

--- test/importSR_tests.jl
+++ test/importSR_tests.jl
@@ -3,7 +3,8 @@ using Test
 using DataFrames
 
 @testset "importSR.jl" begin
-    @test typeof(importSR("/Users/Patrick/Documents/GitHub/SuperResolution/src/locs_gamma_conf.csv","/Users/Patrick/Documents/GitHub/SuperResolution/src/labels_gamma.csv"))==Tuple{DataFrame,DataFrame}
+    @test typeof(SuperResolution.DataSim)==DataFrame
+    @test typeof(SuperResolution.RealLocs)==DataFrame
     # Load Data with colnames:
     # mol_ID|loc_ID|framenumber|pos_x|pos_y
     # [Int64,Int64,Int64,Float64,Float64,Float64,Float64,Float64,Float64,Float64]
